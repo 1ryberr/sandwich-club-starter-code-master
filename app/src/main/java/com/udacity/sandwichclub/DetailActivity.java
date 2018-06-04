22f670bfb2b27ed5f660e35aff7919db1e1b8d03
Ryan Berry <ryber8@aol.com>
2018-06-04 12:09:04 -0400

    fix: Implemented a placeholder

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -60,7 +60,9 @@ public class DetailActivity extends AppCompatActivity {
         populateUI();
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.mipmap.ic_launcher)
                 .into(ingredientsIv);
+
 
         setTitle(sandwich.getMainName());
     }
